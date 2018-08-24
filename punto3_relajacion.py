7231b3faeb17507578c7fa0bdec4fc1a1fdcfaf6
oscarfonsecaneira <41377791+oscarfonsecaneira@users.noreply.github.com>
2018-08-24 08:46:26 -0500

    Update punto3_relajacion.py

--- punto3_relajacion.py
+++ punto3_relajacion.py
@@ -3,10 +3,10 @@ import numpy as np
 ITERATION_LIMIT = 1000
 
 # initialize the matrix
-A = np.array([[10., -1., 2., 0.],
-              [-1., 11., -1., 3.],
-              [2., -1., 10., -1.],
-              [0., 3., -1., 8.]])
+A = np.array([[4., -1., -1., -1.],
+              [-1., 4., -1., -1.],
+              [-1., -1., 4., -1.],
+              [-1., -1., -1., 4.]])
 # initialize the RHS vector
 b = np.array([0., 0., 0., 0.])
 
